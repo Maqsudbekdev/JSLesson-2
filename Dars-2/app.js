368b4201951afed8e70bed2b4e7c3c9e5b3e1a6d
Maqsudbek <115130682+Maqsudbekdev@users.noreply.github.com>
2022-10-16 14:50:29 +0500

    Add files via upload

---
+++ Dars-2/app.js
@@ -0,0 +1,73 @@
+1 - usul;
+alert("Hello this site is dangerous!");
+
+2 - usul;
+confirm("Accept all cookies?");
+
+3 - usul;
+prompt("Please enter your age!");
+
+(""); // string
+3; // number
+
+console.log(4 + 4);
+console.log(3288748 - 82638);
+
+console.log(4 + "2");
+console.log(4 * "2");
+console.log(4 - "2");
+console.log(2 / "2");
+
+console.log(5 * "hello");
+console.log(5 - "hello"); // NaN = Not a Number
+
+var nimadir = "Hello ";
+console.log(nimadir + "Jaloliddin");
+
+var foydalanuvchiNomi = "Jaloliddin";
+var foydalanuvchiYoshi = 17;
+console.log(
+  "Salom, " + foydalanuvchiNomi + "." + "Yoshingiz " + foydalanuvchiYoshi + "da"
+);
+
+var box1 = "wjhddkjffkjh";
+let box2 = "jahjjjaasjsi";
+
+//  let var dan kora koproq ishlatiladi
+
+console.log(typeof foydalanuvchiYoshi); // typeof operator
+
+var laptop = ["Asus TUF gaming F15", "Macbook Pro"]; // [] => array / massive
+// console.log(laptop);
+
+function test(laptop) {
+  var n = laptop.indexOf(" ");
+  var res = laptop.substring(n + 1, -1);
+  return res;
+}
+
+alert("this site is dangerous");
+prompt("What's your ");
+confirm("hello enter your password");
+var nimadir = "hello";
+console.log(nimadir + " Jalolidddin");
+
+console.log(4 + 4);
+console.log(4 + "6");
+console.log(4 / "7");
+console.log(5 - "66");
+
+var userName = "Jaloliddin";
+var userAge = 17;
+console.log("Hello " + userName + " .You are " + userAge + " years old");
+console.log(typeof userAge);
+
+var laptops = [
+  " Macbook Pro",
+  "Asus Tuf Gaming ",
+  "Asus ",
+  "Lenovo Yoga",
+  "Samsung",
+];
+
+console.log(userName);
